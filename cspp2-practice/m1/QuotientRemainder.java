cc1d7556e3fa24a280ee8b91cbfd1caa1c5edf45
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-27 21:03:00 +0530

    QuoRem

---
+++ cspp2-practice/m1/QuotientRemainder.java
@@ -0,0 +1,10 @@
+public class QuotientRemainder
+{
+	public static void main(String[] args)
+	{
+		int a=2,b=3,quo,rem;
+		quo = a/b;
+		rem = a%b;
+		System.out.println("rem: "+a+ " and " +"quo: "+b);
+	}
+}

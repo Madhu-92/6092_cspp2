f0e31b826c75a8c41a748eac8d5900a602f42136
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-28 19:51:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m2/Assignment-3/Solution.java
+++ cspp2-assignments/m2/Assignment-3/Solution.java
@@ -35,10 +35,10 @@ public final class Solution {
         */
         if (exponent == 0) {
              return 1;
-         }else if (exponent == 1) {
+         } else if (exponent == 1) {
              return base;
-         }else {
-            return base * power(base, exponent - 1);
+         } else {
+             return base * power(base, exponent - 1);
         }
     }
 }

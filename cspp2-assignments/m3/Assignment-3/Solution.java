77891cf4507fc804cd2ea2201ffb091aa48effcd
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-29 15:33:03 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ cspp2-assignments/m3/Assignment-3/Solution.java
@@ -0,0 +1,48 @@
+/**
+* Scanner class.
+*/
+import java.util.Scanner;
+/**
+*Do not modify this main function.
+*Solution class
+*/
+public class Solution {
+    /**
+    *constructor
+    */
+    private Solution() {
+        /**
+        *this is a constructor.
+        */
+    }
+    /** this is method.
+    *@param args String
+    */
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);      
+        int n1 = s.nextInt();
+        int n2 = s.nextInt();
+        System.out.println(gcd(n1, n2));
+    }
+    /**
+	*Need to write the gcd function and print the output.
+    *@param n1 int
+    *@param n2 int
+    *@return power of two arguments
+	*/
+    public static int gcd(int n1, int n2) {
+        /**
+        *gcd method.
+        */
+        if (n1 == 0 || n2 == 0) {
+            return 0;
+        }
+        if  (n1 == n2) {
+            return n1;
+        }
+        if (n1 > n2) {
+            return gcd(n1 - n2, n2);
+        }
+        return gcd(n1, n2 - n1);
+    }
+}

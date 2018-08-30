cc01870ef90e6279cbb6b8a8321dae6ccc4a51d2
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-30 20:28:45 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -1,68 +1,61 @@
-import java.util.*;
-public class Solution {
-    /* Fill the main function to print resultant of addition of matrices*/
-    public static void main(String[] args) {
+/**
+*scanner class.
+*/
+import java.util.Scanner;
+/**
+*solution class.
+*/
+public final class Solution {
+    /**
+    *Fill the main function to print resultant of addition of matrices
+    */
+    /**
+    *constructor.
+    */
+    private Solution() {
+        /**
+        *this is a constructor.
+        */
+    }
+    /** this is main method.
+    *@param args String
+    */
+    public static void main(final String[] args) {
         Scanner s = new Scanner(System.in);
         int m = s.nextInt();
         int n = s.nextInt();
-        
-
         int[][] a = new int[m][n];
-        
-
-        for (int i=0; i<m; i++) { 
-            for (int j=0; j<n; j++) { 
+        for (int i=0; i<m; i++) {
+            for (int j=0; j<n; j++) {
                 a[i][j] = s.nextInt();
-                }
+            }
         }
 
-        // System.out.println("--------------------------");
-        
-        // for (int i=0; i<m; i++) { 
-        //     for (int j=0; j<n; j++) { 
-        //         System.out.print(a[i][j] + " ");
-        //         }
-        //         System.out.println();
-                
-        // }
-        // System.out.println("--------------------------");
         int p = s.nextInt();
         int q = s.nextInt();
         int[][] b = new int[p][q];
-        for (int i=0; i<p; i++) { 
-            for (int j=0; j<q; j++) { 
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < q; j++) {
                 b[i][j] = s.nextInt();
-                }
+            }
         }
-        // System.out.println("--------------------------");
-        // for (int i=0; i<p; i++) { 
-        //     for (int j=0; j<q; j++) { 
-        //         System.out.print(b[i][j] + " ");
-        //         }
-        //         System.out.println();
-                
-        // }
         if (m == p && n == q) {
             int[][] c = new int[m][n];
-            // for(int i=0;i<m;i++){    
-            // for(int j=0;j<n;j++){    
-            //     c[i][j]=a[i][j]+b[i][j];
-            // }
-            // }
-            for(int i=0;i<m;i++){    
-            for(int j=0;j<n-1;j++){    
-                c[i][j]=a[i][j]+b[i][j];
-                System.out.print(c[i][j]+" ");
+            for(int i = 0 ;i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    c[i][j] = a[i][j] + b[i][j];
+                }
             }
-            System.out.println(c[i][n-1]);//new line 
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n - 1; j++) {
+                    c[i][j] = a[i][j] + b[i][j];
+                    System.out.print(c[i][j] + " ");
+            }
+                System.out.println(c[i][n - 1]);
             }
         }
         else{
             System.out.println("not possible");
-        
-            
-        
         }
-        
     }
 }

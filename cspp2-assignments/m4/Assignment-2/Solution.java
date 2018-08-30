c0a620fa26d3483c1f8881170ec7e3b93c7d6287
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-30 20:31:54 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-2/Solution.java
+++ cspp2-assignments/m4/Assignment-2/Solution.java
@@ -25,8 +25,8 @@ public final class Solution {
         int m = s.nextInt();
         int n = s.nextInt();
         int[][] a = new int[m][n];
-        for (int i=0; i<m; i++) {
-            for (int j=0; j<n; j++) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 a[i][j] = s.nextInt();
             }
         }
@@ -41,20 +41,20 @@ public final class Solution {
         }
         if (m == p && n == q) {
             int[][] c = new int[m][n];
-            for(int i = 0 ;i < m; i++) {
-                for(int j = 0; j < n; j++) {
+            for (int i = 0 ;i < m; i++) {
+                for (int j = 0; j < n; j++) {
                     c[i][j] = a[i][j] + b[i][j];
                 }
             }
-            for(int i = 0; i < m; i++) {
-                for(int j = 0; j < n - 1; j++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n - 1; j++) {
                     c[i][j] = a[i][j] + b[i][j];
                     System.out.print(c[i][j] + " ");
-            }
+               }
                 System.out.println(c[i][n - 1]);
             }
         }
-        else{
+        else {
             System.out.println("not possible");
         }
     }

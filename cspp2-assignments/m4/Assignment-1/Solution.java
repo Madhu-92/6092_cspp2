169dcd85b59b77b6d9d57d43cb2dfdc126d3e9df
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-30 11:35:22 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-1/Solution.java
+++ cspp2-assignments/m4/Assignment-1/Solution.java
@@ -1,21 +1,38 @@
-
-import java.util.*;
-public class Solution
-{/*
-	Fill this main function to print maximum of given array
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= sc.nextInt();
-		int a[];
-		int temp=0;
-		a = new int[n];
-		for (int i=0; i<a.length; i++) {
-			a[i]=sc.nextInt();
-			if (temp<a[i]) {
-				temp=a[i];
-			}
-		}System.out.println(temp);
-	}
+/**
+*scanner class.
+*/
+import java.util.Scanner;
+/**
+*solution class
+*/
+public final class Solution {
+    /**
+    *Fill this main function to print maximum of given array
+    */
+    /**
+    *constructor.
+    */
+    private Solution() {
+        /**
+        *this is a constructor.
+        */
+    }
+    /** this is main method.
+    *@param args String
+    */
+    public static void main(final String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        int a[];
+        int temp=0;
+        a = new int[n];
+        for (int i=0; i<a.length; i++) {
+            a[i]=sc.nextInt();
+            if (temp<a[i]) {
+                temp=a[i];
+            }
+        }
+        System.out.println(temp);
+    }
 }

aa4ca56b521b20bd94d19b48d2b0060c6ee903ee
Madhu-92 <madhu.ktej@msitprogram.net>
2018-08-31 11:33:53 +0530

    Concatinate

---
+++ cspp2-practice/m4/Concatinate.java
@@ -0,0 +1,9 @@
+class Concatinate{
+	public static void main(String[] args) {
+		String helloName = "Hello";
+		helloName = helloName.concat(" bob!");
+		// helloName.concat("Alice");
+		// helloName.concat("X");
+		System.out.println(helloName);
+	}
+}

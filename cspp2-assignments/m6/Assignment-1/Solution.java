d37760ff0500b2d7911fe446d218d4805eb4451d
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-01 11:37:18 +0530

    "check_style_score -> 1 of 6 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-1/Solution.java
+++ cspp2-assignments/m6/Assignment-1/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
      * //@param      n     n value.
      */
      //static void oddComposites(final int n) {
-     //write your code here     
+     //write your code here.
      //}
     /**
     * main method as driver program.
@@ -31,12 +31,12 @@ public final class Solution {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         //oddComposites(n);
-        for (int i = 2; i < n; i++) {
-            for (int j = 2; j <= i / 2; j++) {
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= i / 2; j++) {
             if (i % j == 0) {
                System.out.println(i);
                break;
-            } 
+            }
             }
         }
     }

94685c0beca910a7c483da0cb595193d8feca94d
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-03 20:44:11 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m7/Assignment-1/Solution.java
+++ cspp2-assignments/m7/Assignment-1/Solution.java
@@ -1,18 +1,21 @@
-/**
+/**.
  * Scanner class
  */
 import java.util.*;
-/**
+/**.
  * Class for input validator.
  */
 class InputValidator {
+    /**
+     * { item_description }
+     */
     /*Write the atrributes and methods for InputValidator*/
     static String input;
     public InputValidator(String input) {
         this.input = input;
     }
     boolean validateData() {
-        if (input.length() > 6) {
+        if (input.length() >= 6) {
             return true;
         } else {
             return false;
@@ -20,22 +23,26 @@ class InputValidator {
     }
 
 }
-/**
+/**.
  * Class for solution.
  */
 public final class Solution {
-    /**
+    /**.
     * Empty constructor.
     */
     private Solution() {
         //not used.
     }
-    /**
-    * main method as driver program.
-    * @param args is the parameter for this method.
-    */
 
-    public static void main(final String args[]) {
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**
+         * { var_description }
+         */
         Scanner s = new Scanner(System.in);
         String input = s.next();
         InputValidator i = new InputValidator(input);

ab279f3a7985fb012e863e11b9ec2209d9dc323b
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-04 18:13:57 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- cspp2-assignments/m8/CodecampList/List.java
+++ cspp2-assignments/m8/CodecampList/List.java
@@ -11,6 +11,9 @@ import java.util.Scanner;
  */
 public class List {
   //Implement all the methods mentioned to build a ListADT
+  /**
+  * Private int array.
+  */
   private int[] intArray;
 
   /**
@@ -62,15 +65,18 @@ public class List {
   // declare a private int size
   // again, don't initialize it here
   // variable initialization should be done in the constructor
+  /**
+  * private int size.
+  */
   private int size;
   /**
    * The purpose of the constructor is to initialize the
    * class variables with some default values.
    */
   public List() {
+    final int ten = 10;
+    intArray = new int[ten];
     size = 0;
-    intArray = new int[10];
-
 
     // what are the two variables to be initialized here?
     // think about the private variables described above.
@@ -108,8 +114,8 @@ public class List {
 
   /**
    * The size method returns the value of the size.
-   * The purpose of the method is to announce the size of the list
-   * to the objects outside the list
+   * The purpose of the method is to announce the size of the list.
+   * to the objects outside the list.
    *
    * The method returns an int. Empty list should return 0.
    */
@@ -130,15 +136,15 @@ public class List {
    * Think about what happens when
    * an item is removed from the middle of the list
    * It creates a hole in the list, right?
-   * This would mean, all the items that are
-   * to the right side of the removed item should be
+   * This would mean, all the items that are.
+   * to the right side of the removed item should be.
    * moved to the left by one position.
    * Here is an example:
    * array = [1,2,3,0,0,0,0,0,0,0]
    * remove(1) would remove the item 2 which is at index position 1.
    * But how do you remove the item from an array?
-   * Well, the way to remove it is to move all
-   * the items, that are to the right of the removed item, to the left
+   * Well, the way to remove it is to move all.
+   * the items, that are to the right of the removed item, to the left.
    * So, the new array looks like this.
    * array = [1,3,0,0,0,0,0,0,0,0]
    * The method returns void (nothing)
@@ -164,12 +170,12 @@ public class List {
   /**
    * Get method has to return the items that is
    * at the index position passed as an argument to the method.
-   * If the item doesn't exist then return a -1 to indicate that
+   * If the item doesn't exist then return a -1 to indicate that.
    * there is no element at that index.
    * How can an element not be there at a given position?
-   * Well, if the position is greater than the number of items
+   * Well, if the position is greater than the number of items.
    * in the list then that would mean the item doesn't exist.
-   * How do we check if the position is greater than the
+   * How do we check if the position is greater than the.
    * number of items in the list? Would size variable be useful?
    */
   /**
@@ -180,7 +186,7 @@ public class List {
    * @return     { description_of_the_return_value }
    */
   public int get(final int index) {
-    // Replace the code below to write the code for get
+    // Replace the code below to write the code for get.
     for (int i = 0; i < size; i++) {
       if (intArray[i] == intArray[index]) {
         return intArray[index];
@@ -191,21 +197,21 @@ public class List {
 
   /**
    * What happens when you print an object using println?
-   * Java provides a method named toString that is internally
+   * Java provides a method named toString that is internally.
    * invoked when an object variable is used in println.
    * For example:
    * List l = new List();
    * System.out.println(l);
-   * This statement is a shortcut for
+   * This statement is a shortcut for.
    * System.out.println(l.toString());
    *
-   * So, implement the toString method to display the items
+   * So, implement the toString method to display the items.
    * in the list in the square brackets notation.
    * i.e., if the list has numbers 1, 2, 3
    * return the string [1,2,3]
-   * Caution: The array may be having other elements
+   * Caution: The array may be having other elements.
    * Example: [1,2,3,0,0,0,0,0,0,0]
-   * toString should only return the items in the list and
+   * toString should only return the items in the list and.
    * not all the elements of the array.
    *
    */
@@ -234,7 +240,6 @@ public class List {
    * { contains method }
    *
    * @param      item  The item
-   *
    * @return     { description_of_the_return_value }
    */
   public boolean contains(final int item) {

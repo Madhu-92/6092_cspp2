715784360891b8f35c72a758461d0143b628a0f5
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-06 12:04:33 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m10/Assignment-1/List.java
+++ cspp2-assignments/m10/Assignment-1/List.java
@@ -305,12 +305,21 @@ public class List {
    * @param      item   The item
    */
   public void add(final int index, final int item) {
+    if (index < 0) {
+        System.out.println("Negative Index Exception");
+    }
+    if (index == 0 && size == 0) {
+        intArray[index] = item;
+        size++;
+    }
+    if (index > 0) {
     for (int i = size; i > index; i--) {
       intArray[i] = intArray[i - 1];
     }
     intArray[index] = item;
     size++;
   }
+}
 
   /**
    * addsAll method

2f4d779bd7ae78f53c871c682e56e47ea74f7c4d
Madhu-92 <madhu.ktej@msitprogram.net>
2018-09-22 12:59:51 +0530

    "check_style_score -> 3 of 14 passed. style score: 0/1 "

--- cspp2-assignments/m25/Assignment-1/TodoistMain.java
+++ cspp2-assignments/m25/Assignment-1/TodoistMain.java
@@ -26,7 +26,7 @@ public class TodoistMain {
                 //     testAddTask(todo, tokens);
                 // break;
                 case "print-todoist":
-                    System.out.println(todo);
+                    System.out.println(todo.toString());
                 break;
                 // case "get-next":
                 //     System.out.println(todo.getNextTask(tokens[1]));

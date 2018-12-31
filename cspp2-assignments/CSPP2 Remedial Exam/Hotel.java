bd6e5ec303c36eb48161dbf337e8d49573e9e162
Madhu-92 <madhu.ktej@msitprogram.net>
2018-12-31 17:42:37 +0530

    "cspp2_m10_a1_01: 6/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ cspp2-assignments/CSPP2 Remedial Exam/Hotel.java
@@ -0,0 +1,65 @@
+public class Hotel {
+	Reservation[] rooms;
+	int size;
+	int rNum = 1;
+	Reservation reserveObj;
+	Hotel() {
+		rooms = new Reservation[5];
+		size = 0;
+	}
+	public int reserveRoom(String person) {
+		if (size == rooms.length) {
+			System.out.println("All Rooms are reserved");
+		}
+		for (int i = 0; i < rooms.length; i++) {
+			if (rooms[i] == null) {
+				reserveObj = new Reservation(person);
+				reserveObj.setRoom(i + 1);
+				rooms[i] = reserveObj;
+				size++;
+				return i + 1;
+			}
+		}
+		return -1;
+	}
+
+	public boolean reserveRoom(String person, int roomNum) {
+		if (size == rooms.length) {
+			System.out.println("All Rooms are reserved");
+		} else if (rooms[roomNum - 1] == null) {
+			reserveObj = new Reservation(person, roomNum);
+			rooms[roomNum  - 1] = reserveObj;
+			rNum++;
+			size++;
+			return true;
+		} else if (rooms[roomNum - 1] != null) {
+			System.out.println("Room is already reserved");
+		}
+		return false;
+	}
+
+	public void printReservations() {
+		for (int i = 0; i < size; i++) {
+			if (rooms[i] != null) {
+				System.out.println(rooms[i].getName() + " " + rooms[i].getRoom());
+			}
+		}
+	}
+
+	public void cancelReservations(String person) {
+		for (int i = 0; i < size; i++) {
+			if (rooms[i] != null) {
+				if (rooms[i].getName().equals(person)) {
+					rooms[i] = null;
+				}
+			}
+		}
+	}
+	public boolean buildRooms(int num) {
+		Reservation[] temp = new Reservation[rooms.length + num];
+		System.arraycopy(rooms, 0, temp, 0, rooms.length);
+		rooms = temp;
+		return true;
+	}
+
+}
---
+++ cspp2-assignments/CSPP2 Remedial Exam/Reservation.java
@@ -0,0 +1,43 @@
+import java.util.*;
+/*
+  Reservation class, stores the person and room number in the Hotel
+*/
+
+public class Reservation{
+    //instance variables
+    private String name;
+    private int roomNumber;
+
+    //constructors, must supply the name, and optionally a room
+    public Reservation(String person){
+    	this.name = person;
+
+    }
+    public Reservation(String person, int room) {
+        this.name = person;
+        this.roomNumber = room;
+
+    }
+
+    //mutators, set the room number or name
+    public void setRoom(int newroom){
+    	roomNumber = newroom;
+    }
+
+    public void setName(String newname){
+    	name = newname;
+    }
+
+    //accessors, return the room number or name
+    public int getRoom(){
+    	return this.roomNumber;
+    }
+
+    public String getName(){
+    	return this.name;
+    }
+
+    public String toString() {
+        return getName() + ' ' + getRoom();
+    }
+}

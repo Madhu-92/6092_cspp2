77cdf39b85d021d709d9a312e60f0f79815870fe
Madhu-92 <madhu.ktej@msitprogram.net>
2019-01-28 13:04:00 +0530

    "cspp2_m10_a1_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- cspp2-assignments/Wallet/Wallet.java
+++ cspp2-assignments/Wallet/Wallet.java
@@ -1,6 +1,6 @@
 public class Wallet {
 	String name;
-	float amount = 1000;
+	int amount = 1000;
 	// User[] user;
 	Wallet() {
 		this.amount = 1000;
@@ -11,20 +11,20 @@ public class Wallet {
 	public String getName() {
 		return this.name;
 	}
-	public float getAmount() {
+	public int getAmount() {
 		return this.amount;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public void setAmount(float amount) {
+	public void setAmount(int amount) {
 		this.amount = amount;
 	}
 	public String toString() {
 		return this.name+" "+this.amount;
 	}
 
-	// public void credit(String name, float amount) {
+	// public void credit(String name, int amount) {
 
 	// }
 	// public void debit(String name, int amount) {
